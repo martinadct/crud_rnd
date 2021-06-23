dec4586f105db0b4ebc736838bfd25d2a581af13
martinadct <martinat2203@gmail.com>
2021-06-23 17:06:49 +0700

    Commit Pertama

---
+++ home.php
@@ -0,0 +1,5 @@
+<center><br><br><br><br><br><br><br><br>
+<img src="assets/images/sekolah.png" width="200px height="200px" /> <br>
+<font Size="6" face="Helvetica">Academic Information Center</font> <br>
+<font Size="6">Budiman College</font>
+</center>
